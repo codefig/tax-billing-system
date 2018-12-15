4c427fac4b6363d63236c3674c04512bbe688f57
Moshood Alaran <geekyshood@gmail.com>
2018-12-15 14:06:35 +0100

    adde new routes to web file

--- routes/web.php
+++ routes/web.php
@@ -28,5 +28,8 @@ Route::get('/admin/home', 'Auth\AdminController@showHome')->name('admin.home');
 Route::get('/admin/logout', 'Auth\AdminController@logout')->name('admin.logout');
 
 Route::get('/admin/driver/add', 'Auth\AdminController@showAddDriver')->name('admin.driver.add');
+Route::get('/admin/driver/update', 'Auth\AdminController@updateDriver')->name('admin.driver.update');
 Route::get('/admin/payments', 'Auth\AdminController@showPayments')->name('admin.payments.show');
-Route::get('/admin/history', 'Auth\AdminController@showPaymentHistory')->name('admin.payments.history');
+Route::get('/admin/payments/history', 'Auth\AdminController@showPaymentHistory')->name('admin.payments.history');
+
+//Payment routes
