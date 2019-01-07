edaa524e93a76645b5e3b69f7006d2f221cc273c
USER <user@USERs-MacBook-Pro.local>
2019-01-07 11:14:25 +0100

    linted the jcontroler

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,34 +2,32 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Illuminate\Http;
 use Auth;
+use Illuminate\Http;
+use Illuminate\Http\Request;
 
-class AdminController extends Controller
-{
+class AdminController extends Controller {
 
-    public function __construct(){
-        // return "welcome to the admin controller";
-        $this->middleware('guest:admin');
-    }
+	public function __construct() {
+		// return "welcome to the admin controller";
+		$this->middleware('guest:admin');
+	}
 
-    public function showLogin(Request $request){
-        return view('admin.login');
-    }
+	public function showLogin(Request $request) {
+		return view('admin.login');
+	}
 
-    public function loginSubmit(Request $request){
-        // return "this is the login submit function ";
-        $this->validate($request, [
-            'email' => 'required|email', 
-            'password' => 'required',
-        ]);
+	public function loginSubmit(Request $request) {
+		// return "this is the login submit function ";
+		$this->validate($request, [
+			'email' => 'required|email',
+			'password' => 'required',
+		]);
 
-        if(Auth::guard('admin')->attempt(['email' => $request->email, 'password' => $request->password])){
-            return redirect()->route('admin.home');
-        }
-        else{
-            return redirect()->back()->withInput();
-        }
-    }
+		if (Auth::guard('admin')->attempt(['email' => $request->email, 'password' => $request->password])) {
+			return redirect()->route('admin.home');
+		} else {
+			return redirect()->back()->withInput();
+		}
+	}
 }
