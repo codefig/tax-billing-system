7aae616385372ec56e98b17b4845c9e2abe30ecb
Moshood Alaran <geekyshood@gmail.com>
2019-01-07 11:17:40 +0100

    renamed home title

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>SB Admin - Dashboard</title>
+    <title>Admin - Dashboard</title>
     <!-- Bootstrap core CSS-->
     <link href="{{URL::to('vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
 
