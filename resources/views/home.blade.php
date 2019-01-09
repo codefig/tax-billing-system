d97b8a4f93c20ac120162d27f9c7205fb196c7b9
Moshood Alaran <geekyshood@gmail.com>
2019-01-09 03:06:08 +0100

    added profile update notification warning

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -143,45 +143,14 @@
 
           <!-- Icon Cards-->
           <div class="row">
-            <div class="col-xl-3 col-sm-6 mb-3">
-              <div class="card text-white bg-primary o-hidden h-100">
-                <div class="card-body">
-                  <div class="card-body-icon">
-                    <i class="fas fa-fw fa-comments"></i>
-                  </div>
-                  <div class="mr-5">View All Drivers</div>
-                </div>
-                <a class="card-footer text-white clearfix small z-1" href="{{ route('admin.driver.showall') }}">
-                  <span class="float-left">View Details</span>
-                  <span class="float-right">
-                    <i class="fas fa-angle-right"></i>
-                  </span>
-                </a>
-              </div>
-            </div>
-            <div class="col-xl-3 col-sm-6 mb-3">
-              <div class="card text-white bg-warning o-hidden h-100">
-                <div class="card-body">
-                  <div class="card-body-icon">
-                    <i class="fas fa-fw fa-list"></i>
-                  </div>
-                  <div class="mr-5">Add New Driver</div>
-                </div>
-                <a class="card-footer text-white clearfix small z-1" href="{{ route('admin.driver.show') }}">
-                  <span class="float-left">View Details</span>
-                  <span class="float-right">
-                    <i class="fas fa-angle-right"></i>
-                  </span>
-                </a>
-              </div>
-            </div>
+
             <div class="col-xl-3 col-sm-6 mb-3">
               <div class="card text-white bg-success o-hidden h-100">
                 <div class="card-body">
                   <div class="card-body-icon">
                     <i class="fas fa-fw fa-shopping-cart"></i>
                   </div>
-                  <div class="mr-5">Update Driver information</div>
+                  <div class="mr-5">Update Profile</div>
                 </div>
                 <a class="card-footer text-white clearfix small z-1" href="{{ route('admin.driver.showUpdate') }}">
                   <span class="float-left">View Details</span>
@@ -209,6 +178,10 @@
             </div>
           </div>
 
+         <div class="alert alert-danger">
+           <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+           <strong>Note!</strong>  Please Update your profile information to continue ...
+         </div>
 
 
         <!-- Sticky Footer -->
