b23fbae11608b75faa3ba9174d693af802fa4f91
Moshood Alaran <geekyshood@gmail.com>
2019-01-09 04:02:24 +0100

    update user information now working

--- app/Http/Controllers/Auth/UserController.php
+++ app/Http/Controllers/Auth/UserController.php
@@ -29,6 +29,51 @@ class UserController extends Controller {
 	}
 
 	public function updateProfile(Request $request) {
-		return "this is the update profile information";
+		$this->validate($request, [
+			'photograph' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:4096',
+			'owners_name' => 'required',
+			'drivers_name' => 'required',
+			'engine_no' => 'required',
+			'plate_no' => 'required',
+			'chassis_no' => 'required',
+			'licence_no' => 'required',
+			'car_description' => 'required',
+			'mobile_no' => 'required',
+			'nationality' => 'required',
+			'state' => 'required',
+			'lga' => 'required',
+
+		]);
+
+		$user = User::find($request->_id);
+
+		if ($request->hasfile('photograph')) {
+
+			$image = $request->file('photograph');
+			$name = $image->getClientOriginalName();
+			$new_image_name = time() . '-driver-' . $name;
+
+			$image->move(public_path() . '/images/', $new_image_name);
+			$user->photograph = $new_image_name;
+
+		}
+		//Image was uploaded successfully.
+		$user->owners_name = $request->owners_name;
+		$user->drivers_name = $request->drivers_name;
+		$user->engine_no = $request->engine_no;
+		$user->plate_no = $request->plate_no;
+		$user->chassis_no = $request->chassis_no;
+		$user->licence_no = $request->licence_no;
+		$user->car_description = $request->car_description;
+		$user->mobile_no = $request->mobile_no;
+		$user->nationality = $request->nationality;
+		$user->state = $request->state;
+		$user->lga = $request->lga;
+		$user->password = "";
+		$user->is_updated = 1;
+
+		$user->save();
+		$request->session()->flash('success_message', "The driver information has been updated Successfully!");
+		return redirect()->back();
 	}
 }
